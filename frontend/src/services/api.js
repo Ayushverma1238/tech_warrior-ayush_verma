40c5897c2fbe893a2511faeffe4411d357d6b5d2
Lucifer <anuragyad1398@gmail.com>
2026-04-09 12:33:39 +0530

    Updated URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "https://finflow.onrender.com",
+    baseURL: "https://finflow-gg7q.onrender.com",
 });
 
 // Request interceptor
